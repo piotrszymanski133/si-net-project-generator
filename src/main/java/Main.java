f36f3c4799044de36e3d3e16b236fc84178ec25b
piotrszymanski133 <63449627+piotrszymanski133@users.noreply.github.com>
2021-10-30 12:00:00 +0200

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,27 @@
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+    private final static String FIRST_QUEUE_NAME = "temperature";
+
+    public static void main(String[] args) {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        try (Connection connection = factory.newConnection();
+             Channel channel = connection.createChannel()) {
+            channel.queueDeclare(FIRST_QUEUE_NAME, false, false, false, null);
+            while (true) {
+                String message = String.valueOf(Math.random() * 40);
+                channel.basicPublish("", FIRST_QUEUE_NAME, null, message.getBytes());
+                System.out.println("Sent: '" + message + "'");
+                TimeUnit.SECONDS.sleep(1);
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+}
